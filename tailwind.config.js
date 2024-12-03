da5b0e8134daa2e3fd99823b938d744b635faebd
Hatem Soliman <hatemthedev@gmail.com>
2024-12-04 01:33:39 +0200

    Added nativewind

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import("tailwindcss").Config} */
+module.exports = {
+  // NOTE: Update this to include the paths to all of your component files.
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: {
+        "budget-midnight": "#000000",
+        "budget-charcoal": "#101010",
+        "budget-steel": "#2A2A2A",
+        "budget-silver": "#ACACAC",
+        "budget-cloud": "#F5F5F5",
+        "budget-snow": "#FEFEFE",
+        "budget-tangerine": "#F76D35",
+      }
+    },
+  },
+  plugins: [],
+}
