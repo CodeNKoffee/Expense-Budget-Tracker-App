74869d8d88bcda6362379959f0f2797a1bfc98bc
Hatem Soliman <hatemthedev@gmail.com>
2024-12-05 05:29:32 +0200

    Cleaned transactions screen

--- app/(tabs)/transactions.tsx
+++ app/(tabs)/transactions.tsx
@@ -1,31 +1,29 @@
+// React and React hooks
 import React from 'react';
-import { Image, SafeAreaView, ScrollView, StyleSheet, View } from 'react-native';
-import TransactionForm from '@/components/TransactionForm';
-import { ThemedText } from '@/components/shared/ThemedText';
+// Third-party libraries
+import { Image, SafeAreaView, ScrollView, Text, View } from 'react-native';
 import { useTranslation } from 'react-i18next';
+// Components
+import TransactionForm from '@/components/TransactionForm';
+// Assets
 import autoTransactionBanner from '@/assets/images/auto-transaction-banner.png';
 
 export default function TransactionsScreen() {
   const { t } = useTranslation();
 
   return (
-    <SafeAreaView className="flex-1 bg-budget-charcoal">
+    <SafeAreaView className="bg-budget-charcoal flex-1">
       <ScrollView className='mb-16 px-8 py-4'>
         <View className='mb-8 items-center'>
-          <ThemedText style={styles.sectionHeader}>{t('transactions.addTransaction')}</ThemedText>
+          <Text className="text-white text-2xl font-bold pt-4">{t('transactions.addTransaction')}</Text>
         </View>
-        <Image source={autoTransactionBanner} className='w-full h-40' resizeMode='contain' />
+        <Image 
+          source={autoTransactionBanner} 
+          className='w-full h-40' 
+          resizeMode='contain' 
+        />
         <TransactionForm />
       </ScrollView>
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionHeader: {
-    fontSize: 24,
-    color: '#FFF',
-    fontWeight: 'bold',
-    paddingTop: 16,
-  },
-});
